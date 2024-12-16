0fc14ea5b6cb8c0cb0802db626636be683dc3de3
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-12-16 13:03:23 -0500

    feat: Add base FrontEnd and Done Phase 1

---
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,70 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.CategoryDao;
+import org.yearup.data.ProductDao;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
+public class CategoriesController
+{
+    private CategoryDao categoryDao;
+    private ProductDao productDao;
+
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) 
+    {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+
+    // Theese are all CRUD
+    @GetMapping("")
+    public List<Category> getAll()
+    {
+        return categoryDao.getAllCategories();
+    }
+
+    @GetMapping("{id}")
+    public Category getById(@PathVariable int id)
+    {
+        return categoryDao.getById(id);
+    }
+
+    
+    @GetMapping("{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        return productDao.listByCategoryId(categoryId);
+    }
+
+    @PostMapping("")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Category addCategory(@RequestBody Category category)
+    {
+        return categoryDao.create(category);
+    }
+
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void updateCategory(@PathVariable int id, @RequestBody Category category)
+    {
+        categoryDao.update(id, category);
+    }
+
+    @DeleteMapping("{id}") 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteCategory(@PathVariable int id)
+    {
+        categoryDao.delete(id);
+    }
+
+    
+}
